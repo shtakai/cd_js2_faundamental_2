3506d3769746dac3c4c78265f6f8616072967243
Alyson Takai <shtakai@gmail.com>
2016-07-03 14:54:58 -0700

    add

--- prob1.js
+++ prob1.js
@@ -1,7 +1,10 @@
-console.log(first_variable);
-var first_variable = "Yipee I was first!";
+var first_variable;
 function firstFunc() {
-  first_variable = "Not anymore!!!";
-  console.log(first_variable)
+  first_variable = "Not anymore!!!"; // assignment first_variable on line#1
+  console.log(first_variable); // just show first_variable on parent scope
 }
-console.log(first_variable);
+
+//firstFunc(); //< if invoke this, first_variable is overwritten
+console.log(first_variable); // now no assignment, so undefined
+first_variable = "Yipee I was first!";
+console.log(first_variable); // assignment on #9
--- prob2.js
+++ prob2.js
@@ -1,9 +1,15 @@
-var food = "Chicken";
+var food
+food = "Chicken";
+
 function eat() {
+  var food;
   food = "half-chicken";
-  console.log(food);
-  var food = "gone";       // CAREFUL!
-  console.log(food);
+  console.log(food); // < food inside eat()
+  food = "gone";       // CAREFUL!  modified food inside eat()
+  console.log(food); // < food inside eat()
 }
-eat();
-console.log(food);
+
+
+
+eat(); // food in eat is modified, but not modified in this scope
+console.log(food); // < food this scope(not inside eat()
